7a071fd2761dfd7e539972ca5e5308f3b3066fff
Daniel Gostev <120301559+DanielGostev@users.noreply.github.com>
2023-03-12 17:25:59 +0100

    Add files via upload

---
+++ CP1/secrets.py
@@ -0,0 +1,17 @@
+secrets = {
+    'ssid' : 'FRITZ!Box 6591 Cable AC',
+    'password' : '68243341464554296633',
+    'broker' : '192.168.178.35',
+    'user' : 'Daniel',
+    'pass' : 'wavelabs',
+    'port' : 1883
+    }
+   
+#secrets = {
+#    'ssid' : 'Free Viruses',
+#    'password' : 'birazavsichki',
+#    'broker' : '192.168.45.19',
+#    'user' : 'Daniel',
+#    'pass' : 'wavelabs',
+#    'port' : 1883
+#    }
