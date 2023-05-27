994258ced68d4744150d8c4c64c2cb7dd1b7e4bf
Daniel Gostev <120301559+DanielGostev@users.noreply.github.com>
2023-05-27 15:23:10 +0200

    Delete CP directory

--- CP/secrets.py
+++
@@ -1,9 +0,0 @@
-secrets = {
-    'ssid' : 'FRITZ!Box 6591 Cable AC',
-    'password' : '68243341464554296633',
-    'broker' : '192.168.178.35',
-    'user' : 'Daniel',
-    'pass' : 'wavelabs',
-    'port' : 1883
-    }
-   
